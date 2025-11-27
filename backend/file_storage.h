319aad1cd9cd1d9092a06a2790f07182147d0004
gagan0555 <165394260+gagan0555@users.noreply.github.com>
2025-11-27 22:30:41 +0530

    Update file_storage.h

--- backend/file_storage.h
+++ backend/file_storage.h
@@ -4,14 +4,12 @@
 #include "recipe_manager.h"
 
 #define RECIPES_FILE "recipes.dat"
-#define BACKUP_FILE "recipes_backup.dat"
 
 // Function declarations
 int saveRecipesToFile(RecipeManager* manager, const char* filename);
 int loadRecipesFromFile(RecipeManager* manager, const char* filename);
 int exportRecipesToText(RecipeManager* manager, const char* filename);
 int importRecipesFromText(RecipeManager* manager, const char* filename);
-int createBackup(RecipeManager* manager);
-int restoreFromBackup(RecipeManager* manager);
 
 #endif
+
